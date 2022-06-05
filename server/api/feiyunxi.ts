980b225f87b901520da54c890d01b3e11cffbbda
cawa-93 <kozackunisoft@gmail.com>
2022-06-05 11:33:53 +0300

    Виправлення апі

--- server/api/feiyunxi.ts
+++ server/api/feiyunxi.ts
@@ -3,5 +3,5 @@ import * as fs from "node:fs/promises";
 import { pathToFileURL } from 'node:url';
 
 export default defineEventHandler(() => {
-    return fs.readdir('./public/images/feiyunxi')//.then(f => f.map(pathToFileURL))
+    return fs.readdir('.')//.then(f => f.map(pathToFileURL))
 })
