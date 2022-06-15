cd23822c6e303fe92e128d804e138fde5d2f82b8
olena195 <106345885+olena195@users.noreply.github.com>
2022-06-15 14:30:19 +0300

    Update catSet.ts

--- src/catSet.ts
+++ src/catSet.ts
@@ -1,2 +1,2 @@
-export const regular = import.meta.glob('../assets/images/cat.ts/*', {as: 'url', eager: true});
-export const love = import.meta.glob('../assets/images/cat.ts/love/*', {as: 'url', eager: true});
+export const regular = import.meta.glob('../assets/images/cat/*', {as: 'url', eager: true});
+export const love = import.meta.glob('../assets/images/cat/love/*', {as: 'url', eager: true});
